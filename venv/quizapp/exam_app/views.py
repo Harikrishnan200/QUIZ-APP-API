43f8005b038f73da363091cfacb84b26de76d33f
Harikrishnan <haripdm9446718962@gmail.com>
2025-05-07 08:31:34 +0530

    test works

--- venv/quizapp/exam_app/views.py
+++ venv/quizapp/exam_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+class TestView(APIView):
+    def get(self,request):
+        return Response({"message": "Hello, World!"}, status=status.HTTP_200_OK)
