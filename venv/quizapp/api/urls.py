657ced2563efa54be18150838a5ea9bf6d65197f
Harikrishnan <haripdm9446718962@gmail.com>
2025-05-10 23:40:53 +0530

    Authentication code

--- venv/quizapp/api/urls.py
+++ venv/quizapp/api/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from exam_app.views import TestView
+from auth_app.views import RegisterView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', TestView.as_view(), name='test'),
+
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', LoginView.as_view(), name='login'),
 ]
+
+
